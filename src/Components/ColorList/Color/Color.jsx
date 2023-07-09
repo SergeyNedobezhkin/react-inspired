7d0026bd2be8af6edb7d94d702af4a38a1516282
Sergey Nedobezhkin <zelenka1633@gmail.com>
2023-07-09 10:22:46 +0400

    Вывел цвета продуктов

--- src/Components/ColorList/Color/Color.jsx
+++ src/Components/ColorList/Color/Color.jsx
@@ -1,28 +1,19 @@
 import React, { useEffect, useRef } from "react";
 import style from "./Color.module.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { setColor } from "../../../features/colorSlice";
 import cn from "classnames";
-import { NavLink } from "react-router-dom";
 
 function Color({ color, check }) {
-  // const dispatch = useDispatch();
-  const colorCircleRef = useRef(check);
-
-  const onChangeColor = (color, check) => {
-    colorCircleRef.current.classList.toggle(style.colorCheck);
-  };
+  const colorRef = useRef(check);
+  useEffect(() => {
+    colorRef.current.style.setProperty("--data-color", color);
+  }, [color]);
 
   return (
     <li
-      ref={colorCircleRef}
-      style={{ backgroundColor: color }}
-      className={check ? cn(style.color, style.colorCheck) : style.color}
-      // className={style.color}
+      ref={colorRef}
+      className={cn(style.color, check ? style.colorCheck : "")}
       onClick={() => onChangeColor(color, check)}
-    >
-      {/* {color} */}
-    </li>
+    ></li>
   );
 }
 
